cfa119c3048ac63a4c34e8fbb72c9df3f8cb96a1
LusineGhalechyan <57481746+LusineGhalechyan@users.noreply.github.com>
2021-06-27 10:09:51 +0400

    Update hour_HourToDayConvertor.js

--- src/helper/hour_HourToDayConvertor.js
+++ src/helper/hour_HourToDayConvertor.js
@@ -1,6 +1,6 @@
 export default function hour_hourToDayConvertor(tweetTime) {
   const time = (new Date().getTime() - tweetTime) / 1000 / 3600;
   return parseInt(time) >= 24
-    ? `${parseInt(time) / 24}${"d."}`
+    ? `${parseInt(time/ 24)}${"d."}`
     : `${parseInt(time)}${"h."}`;
 }
